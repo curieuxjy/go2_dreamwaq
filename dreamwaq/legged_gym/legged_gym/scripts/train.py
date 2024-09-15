7e27418e2b3c486f02aea0ad7cb0d657746ce0b7
Jungyeon Lee <curieuxjy@gmail.com>
2024-09-16 07:23:38 +0900

    Update std

--- dreamwaq/legged_gym/legged_gym/scripts/train.py
+++ dreamwaq/legged_gym/legged_gym/scripts/train.py
@@ -66,7 +66,7 @@ def train(args):
 
     if WANDB:
         wandb.init(
-            project="main", entity="dreamwaq", config={**env_cfg_dict, **train_cfg_dict}
+            project="dreamwaq", entity="curieuxjy", config={**env_cfg_dict, **train_cfg_dict}
         )
         wandb.tensorboard.patch(save=False, tensorboard_x=True)
         # set run name
